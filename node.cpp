513116c12589a85d6a255e21f8aa608f57775d6e
gallyamov2001 <70097595+gallyamov2001@users.noreply.github.com>
2020-08-23 16:27:19 +0300

    Add files via upload

---
+++ node.cpp
@@ -0,0 +1,56 @@
+#include "node.h"
+
+template<class T>
+bool compare(Comparison cmp, const T& a, const T& b)
+{
+    switch (cmp)
+    {
+        case Comparison::GreaterOrEqual:
+            return a >= b;
+
+        case Comparison::Equal:
+            return a == b;
+
+        case Comparison::NotEqual:
+            return a != b;
+
+        case Comparison::Less:
+            return a < b;
+
+        case Comparison::LessOrEqual:
+            return a <= b;
+
+        case Comparison::Greater:
+            return a > b;
+    }
+
+    throw invalid_argument("Invalid  compare");
+}
+
+bool DateComparisonNode::Evaluate(const Date& date, const string& event) const
+{
+    (void) event;
+    return compare(cmp_, date, date_);
+}
+
+bool EventComparisonNode::Evaluate(const Date& date, const string& event) const
+{
+    (void) date;
+    return compare(cmp_, event, event_);
+}
+
+bool LogicalOperationNode::Evaluate(const Date& date, const string& event) const
+{
+    (void) date; (void) event;
+
+    switch(logic_op)
+    {
+        case LogicalOperation::Or:
+            return left_->Evaluate(date, event) || right_->Evaluate(date, event);
+
+        case LogicalOperation::And:
+            return left_->Evaluate(date, event) && right_->Evaluate(date, event);
+    }
+
+    throw invalid_argument("Invalid logic operator");
+}
